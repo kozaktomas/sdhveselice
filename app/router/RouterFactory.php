47e437c01fddbc14cc8d073b34aa7350ac5588d7
Tomas Kozak <kozak@talko.cz>
2017-04-02 22:16:10 +0200

    Fixed router

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -19,21 +19,15 @@ class RouterFactory
     {
         $router = new RouteList();
         $router[] = new Route('admin', "Sign:in");
-
         $router[] = new Route("rss", "Rss:feed");
         $router[] = new Route('clanky.html?stranka=<vp-page>', "Article:list");
+        $router[] = new Route('clanek/<url>.html', "Article:detail");
         $router[] = new Route("", "Document:default");
         $router[] = new Route("clenove-vyboru.html", "Document:vybor");
         $router[] = new Route("zavody-ve-veselici.html", "Document:zavody");
         $router[] = new Route("sportovni-druzstvo.html", "Document:sport");
         $router[] = new Route("kontakty.html", "Document:kontakt");
         $router[] = new Route("prace-s-mlazedi.html", "Document:mladez");
-        $router[] = new Route("administrace/soubory", "File:default");
-        $router[] = new Route("administrace/odhlasit", "Sign:out");
-        $router[] = new Route("administrace/soubory/smazat/<file>/pom", "File:delete");
-        $router[] = new Route("administrace/clanek/<id_article>/", "Article:admin");
-        $router[] = new Route("administrace/clanek/smazat/<id_article>", "Article:delete");
-        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
         return $router;
     }
 
